9e04503ab63d45df277e6d07d08f035d3ab8772d
Miguel Higorre <miguelh100@gmail.com>
2025-06-07 23:16:58 -0400

    Agreagr multiplicacion y didvision

--- calculadora.js
+++ calculadora.js
@@ -8,3 +8,14 @@ function restar(a, b) {
 
 sumar(5, 3); // 8
 restar(5, 3); // 2
+
+function multiplicar(a, b) {
+    return a * b;
+}
+
+function dividir(a, b) {
+    if (b === 0) {
+        throw new Error("No se puede dividir por cero");
+    }
+    return a / b;
+}
